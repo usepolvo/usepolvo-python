74c8dbb4f95a3c20e40c5519fe8da57248b51954
Caio Pizzol <caio@harbourshare.com>
2024-07-29 06:36:28 -0300

    chore: add api key arg to client + customers props

--- src/usepolvo/stripe/__init__.py
+++ src/usepolvo/stripe/__init__.py
@@ -9,13 +9,22 @@ from .config import get_settings
 
 
 class StripeClient(BaseClient):
-    def __init__(self):
+    def __init__(self, api_key: str | None = None):
         super().__init__()
         settings = get_settings()
-        stripe.api_key = settings.stripe_api_key
+        stripe.api_key = api_key if api_key else settings.stripe_api_key
         self.stripe = stripe
         self.calls = settings.stripe_calls
         self.period = settings.stripe_period
+        self._customers = None
+
+    @property
+    def customers(self):
+        if self._customers is None:
+            from .customers import StripeCustomerClient
+
+            self._customers = StripeCustomerClient(self)
+        return self._customers
 
     def rate_limited_execute(self, method, *args, **kwargs):
         @rate_limited_call(calls=self.calls, period=self.period)
